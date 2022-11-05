de2af9032ebe5b14c40e930652b9c59bba01492e
太阳猫 <91592958+sun-cat@users.noreply.github.com>
2022-11-05 13:34:32 +0800

    Initialize nabeel/capture-host-param

---
+++ README.md
@@ -0,0 +1,5 @@
+# Shopify App Node - Tutorial
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+
+This branch contains one of the steps for the [Build a Shopify app with Node and React](https://developers.shopify.com/tutorials/build-a-shopify-app-with-node-and-react) tutorial.
---
+++ server.js
@@ -0,0 +1,85 @@
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const Koa = require('koa');
+const next = require('next');
+const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
+const {verifyRequest} = require('@shopify/koa-shopify-auth');
+const {default: Shopify, ApiVersion} = require('@shopify/shopify-api');
+const Router = require('koa-router');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
+
+dotenv.config();
+
+Shopify.Context.initialize({
+  API_KEY: process.env.SHOPIFY_API_KEY,
+  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+  SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
+  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October20,
+  IS_EMBEDDED_APP: true,
+  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+});
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev: dev});
+const handle = app.getRequestHandler();
+
+const ACTIVE_SHOPIFY_SHOPS = {};
+
+function extractHostParameter(ctx) {
+  const parts = new URL(`https://${ctx.request.header.host}${ctx.request.url}`);
+  return parts.searchParams.get('host');
+}
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+  server.keys = [Shopify.Context.API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      async afterAuth(ctx) {
+        const {shop, scope, accessToken} = ctx.state.shopify;
+        const host = extractHostParameter(ctx);
+        
+        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+
+        const returnUrl = `https://${Shopify.Context.HOST_NAME}/?host=${host}&shop=${shop}`;
+        const subscriptionUrl = await getSubscriptionUrl(accessToken, shop, returnUrl);
+        ctx.redirect(subscriptionUrl);
+      },
+    }),
+  );
+
+  router.post("/graphql", verifyRequest({returnHeader: true}), async (ctx, next) => {
+    await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
+  });
+
+  const handleRequest = async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  };
+
+  router.get("/", async (ctx) => {
+    const shop = ctx.query.shop;
+
+    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
+      ctx.redirect(`/auth?shop=${shop}`);
+    } else {
+      await handleRequest(ctx);
+    }
+  });
+
+  router.get("(/_next/static/.*)", handleRequest);
+  router.get("/_next/webpack-hmr", handleRequest);
+  router.get("(.*)", verifyRequest(), handleRequest);
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
